940828e2342d8bc245242c0fc7618d5cfb63b8ab
revisionismer <cherry2137@naver.com>
2023-06-10 22:14:22 +0900

    gradlew build시 실패 원인 수정

--- src/main/java/com/bank/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/bank/config/auth/PrincipalDetailsService.java
@@ -1,5 +1,7 @@
 package com.bank.config.auth;
 
+import java.util.Optional;
+
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,11 +22,25 @@ public class PrincipalDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
-		User userPS = userRepository.findByUsername(username).orElseThrow(() -> {
-			throw new InternalAuthenticationServiceException("인증 실패");
-		});
+		Optional<User> userOp = userRepository.findByUsername(username);
 		
-		return new PrincipalDetails(userPS);
+		if(userOp.isPresent()) {
+			User userPS = userOp.get();
+			return new PrincipalDetails(userPS);
+		} else {
+			throw new InternalAuthenticationServiceException("인증 실패");
+		}
+		
+		/*
+		 * 
+		  	User userPS = userRepository.findByUsername(username).orElseThrow(() -> {
+				throw new InternalAuthenticationServiceException("인증 실패");
+			});
+		
+			return new PrincipalDetails(userPS);
+			
+			--> 요 방식은 gradlew build때 오류남.
+		 */
 	}
 
 }
