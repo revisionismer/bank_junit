a46bd67acdd605393ca894095d0f6d1fc47a0e74
revisionismer <cherry2137@naver.com>
2023-07-09 21:31:38 +0900

    36. 계좌입금 서비스 만들기

---
+++ src/main/java/com/bank/dto/account/AccountDepositRespDto.java
@@ -0,0 +1,22 @@
+package com.bank.dto.account;
+
+import com.bank.domain.account.Account;
+import com.bank.domain.transaction.Transaction;
+import com.bank.dto.transaction.TransactionDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AccountDepositRespDto {
+
+	private Long id;  // 1-1. 계좌 id
+	private String number;  // 1-2. 계좌 번호
+	private TransactionDto transaction;  // 1-3. 거래내역 
+	
+	public AccountDepositRespDto(Account account, Transaction transaction) {
+		this.id = account.getId();
+		this.number = account.getNumber();
+		this.transaction = new TransactionDto(transaction);
+	}
+}
