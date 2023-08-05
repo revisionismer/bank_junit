369e9282826866eff73a3df8b3d19dc68b06d004
revisionismer <cherry2137@naver.com>
2023-08-05 16:53:19 +0900

    53. @DataJpaTest 더티체킹

--- src/main/java/com/bank/domain/transaction/TransactionRepository.java
+++ src/main/java/com/bank/domain/transaction/TransactionRepository.java
@@ -2,6 +2,6 @@ package com.bank.domain.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, Dao{
 
 }
